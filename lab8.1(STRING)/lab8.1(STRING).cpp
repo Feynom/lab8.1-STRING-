a3f6b7208fdfbda374445aa0875bb3664d1262b0
Feynom <chreptak12@gmail.com>
2020-11-18 11:03:29 +0200

    1

---
+++ lab8.1(STRING)/lab8.1(STRING).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+int kilkist(const string str);
+string modified(string& str);
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string str;
+
+    cout << "Введіть строку: ";
+    getline(cin, str);
+
+    cout << "Число входжень в послідовність групи букв 'while': " << kilkist(str) << endl;
+    cout << "Модифікована строка: " << modified(str) << endl;
+
+    return 0;
+}
+int kilkist(const string str)
+{
+    int k = 0;
+    size_t pos = 0;
+    while((pos = str.find("while", pos, 5)) < str.size())
+    {
+            k++;
+            pos += 5;
+    }
+    return k;
+}
+string modified(string& str)
+{
+    size_t pos = 0;
+    while ((pos = str.find("while", pos, 5)) < str.size())
+        str.replace(pos, 5, "***");
+    return str;
+}
